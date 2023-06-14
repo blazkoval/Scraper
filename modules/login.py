344022903356f68b9933eef1250708a34c6cbd8a
blazkoval <blazkovalenka2@gmail.com>
2023-06-14 15:49:14 +0200

    created, login added, modules

---
+++ modules/login.py
@@ -0,0 +1,24 @@
+import base64
+from asyncio import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+def login(driver):
+
+    try:
+        f = open("..\login.txt", "r")
+        username = f.readline()
+        password = f.readline()
+        password = base64.b64decode(password.encode('ascii')).decode('ascii')
+        f.close()
+    except:
+        print("File not found.")
+
+    elem = driver.find_element(By.NAME, "Username")
+    elem.clear()
+    elem.send_keys(username)
+    elem = driver.find_element(By.NAME, "Password")
+    elem.clear()
+    elem.send_keys(password)
+    elem.send_keys(Keys.RETURN)
