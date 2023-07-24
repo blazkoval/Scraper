2f9502aa3c07103c6eac43986b2fb40f7349cb8b
blazkoval <blazkovalenka2@gmail.com>
2023-07-24 11:48:04 +0200

    zkouším další elemnty, problém s timeout

--- main.py
+++ main.py
@@ -15,15 +15,48 @@ login(driver)
 driver.maximize_window()
 openRozvrh(driver)
 
+"""
+Zkouším vypsat informace z rozvrhu
+nejdříve nadpis léto 2022/2023
+
+potom celé políčko:
+    přes XPATH --> nefunguje (zakomentováno)
+    přes CLASS_NAME --> funguje
+
+potom jeden konrétní řádek
+"""
+#nadpis léto 2022/2023
 try:
-    #jedno políčko v rozvhru
-    #elem = WebDriverWait(driver, timeout=40).until(lambda d: d.find_element(By.XPATH,'/html/body/div/div/div[2]/div[2]/div[2]/div[4]/div/svg/g/g[1]/g'))
-    elem = WebDriverWait(driver, timeout=40).until(lambda d: d.find_element(By.CLASS_NAME,'event-description'))
-    print("Element nalezen")
+    elem = WebDriverWait(driver, timeout=120).until(lambda d: d.find_element(By.XPATH,'/html/body/div/div/div[2]/div[1]/div[2]/button'))
+    print("Nadpis nalezen")
+    print(elem.text)
 except:
-    print("Element NEnalezen")
-#elem = WebDriverWait(driver, timeout=40).until(lambda d: d.find_element(By.XPATH,'/html/body/div/div/div[2]/div[2]/div[2]/div[4]/div/svg/g/g[6]/g/text[1]'))
-print(elem.text)
+    print("Nadpis NEnalezen")
+
+#jedno políčko v rozvhru
+try:
+    #elem = WebDriverWait(driver, timeout=120).until(lambda d: d.find_element(By.XPATH,'/html/body/div/div/div[2]/div[2]/div[2]/div[4]/div/svg/g/g[1]/g'))
+    elem = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.CLASS_NAME,'event-description'))
+    print("Políčko nalezeno")
+    print(elem.text)
+except:
+    print("Políčko NEnalezeno")
+
+#jeden konkretní řádek v políčku - vyučující --> nefunguje
+try:
+    elem = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.CLASS_NAME,'text.event-text.who'))
+    print("Řádek nalezen")
+    print(elem.text)
+except:
+    print("Řádek NEnalezen (1)")
+
+#jeden konkretní řádek v políčku - vyučující --> nefunguje
+try:
+    elem = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.CLASS_NAME,'event-text who'))
+    print("Řádek nalezen")
+    print(elem.text)
+except:
+    print("Řádek NEnalezen (2)")
 
 # source = driver.page_source
 # print(source)
